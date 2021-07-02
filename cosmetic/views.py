ef3801d5d91ad3e1f55d4e20536ce5f4a628b8a6
mincheol <maxandy@naver.com>
2021-07-02 15:52:01 +0900

    박민철 완료

--- cosmetic/views.py
+++ cosmetic/views.py
@@ -9,3 +9,11 @@ def find_foundation(requset):
     return render(requset, 'find_foundation.html')
 def foundation_result(requset):
     return render(requset, 'foundation_result.html')
+def mens_ranking(request):
+    return render(request,'mens_ranking.html')
+def mens_rankingdetail1(request):
+    return render(request,'mens_rankingdetail1.html')
+def mens_rankingdetail2(request):
+    return render(request,'mens_rankingdetail2.html')
+def search_result(request):
+    return render(request,'search_result.html')
--- mensskin/urls.py
+++ mensskin/urls.py
@@ -8,4 +8,8 @@ urlpatterns = [
     path('',cosmetic.views.home, name="home"),
     path('find_foundation/',cosmetic.views.find_foundation, name="find_foundation"),
     path('foundation_result/',cosmetic.views.foundation_result, name="foundation_result"),
+    path('mens_ranking/',cosmetic.views.mens_ranking, name="mens_ranking"),
+    path('mens_rankingdetail1/',cosmetic.views.mens_rankingdetail1, name="mens_rankingdetail1"),
+    path('mens_rankingdetail2/',cosmetic.views.mens_rankingdetail2, name="mens_rankingdetail2"),
+    path('search_result/',cosmetic.views.search_result, name="search_result"),
 ]
