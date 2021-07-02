513bc22b5f496f653ecec783bf3670c4b533a52b
jaejung <wowjd2524@gmail.com>
2021-07-02 10:53:43 +0900

    main and foundation 2 activate submit

--- cosmetic/views.py
+++ cosmetic/views.py
@@ -5,3 +5,7 @@ from django.shortcuts import render
 def home(requset):
     return render(requset, 'home.html')
     # render는 3개의 인자를 받을 수 있음, 3번째는 사전형 객체
+def find_foundation(requset):
+    return render(requset, 'find_foundation.html')
+def foundation_result(requset):
+    return render(requset, 'foundation_result.html')
--- mensskin/urls.py
+++ mensskin/urls.py
@@ -5,5 +5,7 @@ import cosmetic.views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',cosmetic.views.home, name="home")
+    path('',cosmetic.views.home, name="home"),
+    path('find_foundation/',cosmetic.views.find_foundation, name="find_foundation"),
+    path('foundation_result/',cosmetic.views.foundation_result, name="foundation_result"),
 ]
